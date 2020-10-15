6fec78c2d5ed3f931f36b68a01ae4cd8d801a9cd
Peppah0 <61050270@kmitl.ac.th>
2020-10-15 15:55:30 +0700

    first commit

---
+++ test/lottery.test.js
@@ -0,0 +1,29 @@
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const fs = require("fs");
+const Web3 = require("web3");
+const web3 = new Web3(ganache.provider());
+const bytecode = fs.readFileSync('./build/__contracts_lottery_sol_Lottery.bin');
+const abi = JSON.parse(fs.readFileSync('./build/__contracts_lottery_sol_Lottery.abi'));
+var accounts;
+var lottery;
+beforeEach(async () => {
+accounts = await web3.eth.getAccounts()
+lottery = await
+new web3.eth.Contract(abi)
+.deploy({
+data: '0x'+bytecode,
+
+}).send({
+from: accounts[0],
+gas:'1000000'
+});
+
+//console.log(accounts);
+});
+describe('Lottery',() => {
+it('deploys a lottery contract', () => {
+assert.ok(lottery.options.address);
+});
+
+});
